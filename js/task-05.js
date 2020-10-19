1583824d3297f0ce7a17407a65299089690a018c
Liubomyr <ljubik@ukr.net>
2020-10-19 10:18:36 +0300

    Task #05 complete

--- js/task-05.js
+++ js/task-05.js
@@ -1,56 +1,54 @@
 // Завдання 5
-// Задача 3-5
-// Оператор in и метод push
-// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства.
+// Задача 4-5
+// bind для замены this в методах объекта
+// Оформи вызов метода invokeInventoryAction таким образом, чтобы в качестве this для методов
 
-// Функция возвращает массив значений определенного свойства prop из каждого объекта в массиве.
-
-// Используй метод push для добавления значения в массив и оператор in для проверки
-// наличия свойства в объекте.
+// inventory.add
+// inventory.remove выступал объект inventory
 
 // Здано:
-function getAllPropValues(array, prop) {
-	"use strict"
-	// Write code under this line
-
-	return array.reduce(function (result, item) {
-		if (typeof item[prop] != "undefined") result.push(item[prop])
-		return result
-	}, [])
-}
-
-// Объекты и ожидаемый результат
-const products = [
-	{ name: "Радар", price: 1300, quantity: 4 },
-	{ name: "Радар", price: 1280, quantity: 2 },
-	{ name: "Радар", price: 1320, quantity: 1 },
-	{ name: "Сканер", price: 2700, quantity: 1 },
-	{ name: "Сканер", price: 2500, quantity: 3 },
-	{ name: "Дроид", price: 400, quantity: 7 },
-	{ name: "Захват", price: 1200, quantity: 2 },
-]
-
-console.log(getAllPropValues(products, "name"))
-// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
-
-console.log(getAllPropValues(products, "quantity"))
-// [4, 2, 1, 1, 3, 7, 2]
-
-console.log(getAllPropValues(products, "category"))
-//  []
-
-// function getAllPropValues(array, prop) {
-// 	"use strict"
-// 	// Write code under this line
-// 	//return arr.map(el => el[prop]); //ше один варіант
-
-// 	return array.reduce(function (result, item) {
-// 		if (
-// 			result.indexOf(item[prop]) == -1 && // якщо потрібні унікальні значення
-// 			typeof item[prop] != "undefined"
-// 		)
-// 			// якщо не потрібно
-// 			result.push(item[prop])
-// 		return result
-// 	}, [])
-// }
+const inventory = {
+	items: ['Knife', 'Gas mask'],
+	add(itemName) {
+	  this.items.push(itemName);
+	  return `Adding ${itemName} to inventory`;
+	},
+	remove(itemName) {
+	  this.items = this.items.filter(item => item !== itemName);
+	  return `Removing ${itemName} from inventory`;
+	},
+  };
+  
+  const invokeInventoryAction = function(itemName, action) {
+	const act =  action(itemName);
+	const msg =  `Invoking action on ${itemName}`;
+	return {act, msg};
+  };
+  
+  const invokeAdd = invokeInventoryAction(
+	'Medkit',
+	inventory.add.bind(inventory) // Write code in this line
+  );
+  const arrayAdd = [...inventory.items];
+  /* 
+  //console.log(invokeAdd);
+  //{ act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
+  
+  //console.log(arrayAdd);
+  // ['Knife', 'Gas mask', 'Medkit']
+  */
+  
+  const invokeRemove = invokeInventoryAction(
+	'Gas mask',
+	inventory.remove.bind(inventory)  // Write code in this line
+  );
+  
+  const arrayRemove = [...inventory.items];
+  
+  /* 
+  //console.log(invokeRemove);
+  //{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
+  
+  //console.log(arrayRemove);
+  // ['Knife', 'Medkit']
+  */
